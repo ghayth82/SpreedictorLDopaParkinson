1a90e49bcd683a876b117b2a85d1bfcd952d9781
wkopp <kopp@molgen.mpg.de>
2017-10-29 20:14:28 +0100

    added datasets:
    - added metadataset
    - added task 'all' for all individual tasks within a subchallenge
    - changed tremorScore labels from one_hot to integer score

--- code/ldopa/datamanagement/datasets.py
+++ code/ldopa/datamanagement/datasets.py
@@ -3,38 +3,53 @@ from numpydataset import *
 from rawdata import RawData
 from filtereddata import FilteredData
 
-tremor_tasks = ['drnkg', 'fldng',
-                'ramr1', 'raml1', 'ramr2', 'raml2',
-                'orgpa',
-                'ftnl1', 'ftnr1', 'ftnl2', 'ftnr2',
-                'ntblt']
-
-dyskin_tasks = ['ramr1', 'raml1', 'ramr2', 'raml2',
-                'ftnl1', 'ftnr1', 'ftnl2', 'ftnr2']
-
-brakin_tasks = ['drnkg', 'fldng', 'orgpa',
-               'ramr1', 'raml1', 'ramr2', 'raml2',
-               'ftnl1', 'ftnr1', 'ftnl2', 'ftnr2']
-
 def get_dataset_names(data_transform):
-    return ['-'.join(x) for x in itertools.product([data_transform], ['tre'], tremor_tasks)] \
-        + ['-'.join(x) for x in itertools.product([data_transform], ['dys'], dyskin_tasks)] \
-        + ['-'.join(x) for x in itertools.product([data_transform], ['bra'], brakin_tasks)]
+    return ['-'.join(x) for x in itertools.product([data_transform],
+        ['tre'], tremor_tasks)] \
+        + ['-'.join(x) for x in itertools.product([data_transform],
+        ['dys'], dyskin_tasks)] \
+        + ['-'.join(x) for x in itertools.product([data_transform], ['bra'],
+        brakin_tasks)]
 
 # dataset prefixes:
 # - r               raw
 # - fh_[cutoff]     high-pass filter, [cutoff] = cutoff frequency
 # - fl_[cutoff]     low-pass filter, [cutoff] = cutoff frequency
 # - fb_[low]_[high] band-pass filter, [low], [high] = cutoff frequencies
-dataset_names = get_dataset_names('raw') \
-                + get_dataset_names('fh_1') \
-                + get_dataset_names('fh_2') \
-                + get_dataset_names('fh_0.5') \
-                + get_dataset_names('fb_1_20')
+dataset_names = get_dataset_names('raw') + \
+                get_dataset_names('fh_0.5') + \
+                get_dataset_names('fb_0.5_20') + \
+                get_dataset_names('fl_20')
 
+dataset = dict(zip(dataset_names, [{'input_1' : x} for x in dataset_names]))
+
+
+
+# some additional dataset
+####
+##
+# All data is used for the models that use metadata + timeseries
+# for all available tasks per subchallenge
+dataset_names = ["raw-tre-all", "raw-bra-all", "raw-dys-all"]
+
+d1 = dict(zip(dataset_names, [{'input_1' : x, 'input_2' : 'raw-bra-meta'} \
+    for x in dataset_names]))
+
+dataset.update(d1)
+
+
+# raw-bra-meta
+#
+# This dataset is used to test how well bradykinesia
+# can be predicted only from meta data ( site, device, deviceside, ...)
+dataset_names = ["raw-bra-meta"]
+
+d1 = dict(zip(dataset_names, [{'input_1' : 'raw-bra-meta'} \
+    for x in dataset_names]))
+
+dataset.update(d1)
 
 # generate "mPower-like" dict from dataset names
-dataset = dict(zip(dataset_names, [{'input_1' : x} for x in dataset_names]))
 
 
 def get_dataset(name, reload_ = False):
--- code/ldopa/datamanagement/numpydataset.py
+++ code/ldopa/datamanagement/numpydataset.py
@@ -12,8 +12,29 @@ from .utils import batchRandomRotation
 datadir = os.getenv('PARKINSON_DREAM_LDOPA_DATA')
 outcome_vars = {'tre': 'tremorScore', 'dys' : 'dyskinesiaScore', 'bra' : 'bradykinesiaScore'}
 
+tremor_tasks = ['drnkg', 'fldng',
+                'ramr1', 'raml1', 'ramr2', 'raml2',
+                'orgpa',
+                'ftnl1', 'ftnr1', 'ftnl2', 'ftnr2',
+                'ntblt']
+
+dyskin_tasks = ['ramr1', 'raml1', 'ramr2', 'raml2',
+                'ftnl1', 'ftnr1', 'ftnl2', 'ftnr2']
+
+brakin_tasks = ['drnkg', 'fldng', 'orgpa',
+               'ramr1', 'raml1', 'ramr2', 'raml2',
+               'ftnl1', 'ftnr1', 'ftnl2', 'ftnr2']
+
+subch_tasklist = {'tre': tremor_tasks, 'dys': dyskin_tasks, 'bra': brakin_tasks }
+
+
+
 class NumpyDataset(object):
     def __init__(self, outcome, task, reload_ = False):
+        """
+        outcome = ["tre", "dys", "bra"]
+        task = [specific tast, all tasks, or metadata]
+        """
 
         self.outcome = outcome
         self.task = task
@@ -27,8 +48,54 @@ class NumpyDataset(object):
 
         self.load()
 
-    def load(self):
+    def loadAllTasks(self):
         if not os.path.exists(self.npcachefile) or self.reload:
+            ld = LDopa()
+
+            subch = self.outcome
+            outcome_var = outcome_vars[subch]
+
+            cdtask = ld.commondescr[(ld.commondescr["task"].isin(subch_tasklist[subch])) & \
+                    ~(ld.commondescr[outcome_var].isnull())]
+            nrows = cdtask.shape[0]
+
+            # lets just always take 2000 timepoints
+            ndatapoints = 2000
+
+            data = np.zeros((nrows, ndatapoints, 3), dtype="float32")
+            keepind = np.ones((nrows), dtype=bool)
+
+            bar = progressbar.ProgressBar()
+
+            for idx in bar(range(nrows)):
+                df = ld.loadfile(cdtask['dataFileHandleId'].iloc[idx])
+
+                if df.empty or df[self.columns].isnull().any().any():
+                    keepind[idx] = False
+                    continue
+
+                df = self.getValues(df)
+                data[idx, :min(df.shape[0], ndatapoints), :] = df[:min(df.shape[0], ndatapoints), :]
+
+            data = data[keepind]
+
+            labels = cdtask[outcome_var].values
+
+
+            #if outcome_var == 'tremorScore':
+                #labels = pd.DataFrame(to_categorical(labels, num_classes=5), index=labels.index)
+                #labels = cdtask[outcome_var].values
+
+            labels = labels[keepind]
+            patients = cdtask["patient"].iloc[keepind].values
+
+            joblib.dump((data, labels, keepind, patients), self.npcachefile)
+
+        self.data, self.labels, self.keepind, self.patient = joblib.load(self.npcachefile)
+
+    def loadSpecificTask(self):
+        if not os.path.exists(self.npcachefile) or self.reload:
+
             ld = LDopa()
 
             outcome_var = outcome_vars[self.outcome]
@@ -60,16 +127,89 @@ class NumpyDataset(object):
 
             data = data[keepind]
 
-            labels = cdtask[outcome_var]
+            labels = cdtask[outcome_var].values
 
-            if outcome_var == 'tremorScore':
-                labels = pd.DataFrame(to_categorical(labels, num_classes=5), index=labels.index)
+            #if outcome_var == 'tremorScore':
+                #labels = pd.DataFrame(to_categorical(labels, num_classes=5), index=labels.index)
 
             labels = labels[keepind]
 
-            joblib.dump((data, labels, keepind), self.npcachefile)
+            annotation = cdtask.iloc[keepind]
 
-        self.data, self.labels, self.keepind = joblib.load(self.npcachefile)
+            patient = annotation["patient"].values
+
+            joblib.dump((data, labels, keepind, patient), self.npcachefile)
+
+        self.data, self.labels, self.keepind, self.patient = joblib.load(self.npcachefile)
+
+
+    def loadMeta(self):
+        if not os.path.exists(self.npcachefile) or self.reload:
+
+            ld = LDopa()
+            subch = self.outcome
+
+            outcome_var = outcome_vars[self.outcome]
+
+
+            cdtask = ld.commondescr[(ld.commondescr["task"].isin(subch_tasklist[subch])) & \
+                    ~(ld.commondescr[outcome_var].isnull())]
+
+            nrows = cdtask.shape[0]
+
+            device_data = cdtask["device"].apply(lambda x: 1 if x=="GENEActiv" else 0)
+            #session_data = ld.commondescr["session"].apply(lambda x: 1 if x==1 else 0)
+            site_data = cdtask["site"].apply(lambda x: 1 if x=="Boston" else 0)
+            tasks = cdtask["task"].values
+
+            tsks = np.unique(tasks)
+
+            df = pd.DataFrame()
+            for t in tsks:
+                df[t] = cdtask["task"].apply(lambda x: 1 if x==t else 0)
+            task_data = df.values
+            visit_data = cdtask["visit"].apply(lambda x: 1 if x==1 else 0)
+            deviceside_data = cdtask["deviceSide"].apply(lambda x: 1 if x=="Right" else 0)
+
+            keepind = np.ones((nrows), dtype=bool)
+
+            bar = progressbar.ProgressBar()
+
+            for idx in bar(range(nrows)):
+                df = ld.loadfile(cdtask['dataFileHandleId'].iloc[idx])
+
+                if df.empty or df["x"].isnull().any().any():
+                    keepind[idx] = False
+
+            data = np.concatenate((device_data[:, np.newaxis],
+                        site_data[:, np.newaxis], task_data,
+                        visit_data[:, np.newaxis],
+                        deviceside_data[:, np.newaxis]), axis=1)
+
+            data = data[keepind]
+
+            labels = cdtask[outcome_var].values
+
+            #if outcome_var == 'tremorScore':
+                #labels = pd.DataFrame(to_categorical(labels, num_classes=5), index=labels.index)
+
+            labels = labels[keepind]
+
+            annotation = cdtask.iloc[keepind]
+
+            patient = annotation["patient"].values
+
+            joblib.dump((data, labels, keepind, patient), self.npcachefile)
+
+        self.data, self.labels, self.keepind, self.patient = joblib.load(self.npcachefile)
+
+    def load(self):
+        if self.task == "all":
+            self.loadAllTasks()
+        elif self.task == "meta":
+            self.loadMeta()
+        else:
+            self.loadSpecificTask()
 
     def getData(self, idx = None, transform = False):
         if type(idx) == type(None):
@@ -103,18 +243,16 @@ class NumpyDataset(object):
 
     @property
     def patient(self):
-        ld = LDopa()
-        outcome_var = outcome_vars[self.outcome]
-        cdtask = ld.commondescr[(ld.commondescr["task"] == self.task) & ~(ld.commondescr[outcome_var].isnull())]
+        return self.pat
 
-        annotation = cdtask.iloc[self.keepind]
-
-        return annotation["patient"].values
+    @patient.setter
+    def patient(self, pat):
+        self.pat = pat
 
 
     @property
     def labels(self):
-        return self.lab.values
+        return self.lab
 
     @labels.setter
     def labels(self, lab):
@@ -126,3 +264,5 @@ class NumpyDataset(object):
 
     def __len__(self):
         return self.data.shape[0]
+
+
