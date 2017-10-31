4b6734112a7847d5055adf5c583b481793ef3fe4
wkopp <kopp@molgen.mpg.de>
2017-10-31 17:06:53 +0100

    test dys, bra and tre with logreg model on metadata only

--- code/ldopa/logreg_meta.py
+++ code/ldopa/logreg_meta.py
@@ -1,8 +1,11 @@
 from datamanagement.rawdata import RawData
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, accuracy_score
+import numpy as np
 
-rd=RawData("bra","meta")
+sub ="bra"
+
+rd=RawData(sub,"meta")
 
 X = rd.getData()
 y = rd.labels
@@ -10,6 +13,39 @@ y = rd.labels
 cl = LogisticRegression()
 
 cl.fit(X,y)
-score = cl.decision_function(X)
+score = cl.predict(X)
+print("LogReg -{}- AUC={}".format(sub, roc_auc_score(y, score)))
+print("LogReg -{}- acc={}".format(sub, accuracy_score(y, score)))
 
-print("LogReg - AUC={}".format(roc_auc_score(y, score))
+print('#'*85)
+sub ="dys"
+
+rd=RawData(sub,"meta")
+
+X = rd.getData()
+y = rd.labels
+
+cl = LogisticRegression()
+
+cl.fit(X,y)
+score = cl.predict(X)
+print("LogReg -{}- AUC={}".format(sub, roc_auc_score(y, score)))
+print("LogReg -{}- acc={}".format(sub, accuracy_score(y, score)))
+
+####################################
+print('#'*85)
+
+sub ="tre"
+
+rd=RawData(sub,"meta")
+
+X = rd.getData()
+y = rd.labels
+
+cl = LogisticRegression()
+
+cl.fit(X,y)
+#score = cl.decision_function(X)
+score=np.argmax(cl.predict_proba(X),axis=1)
+
+print("LogReg -{}- AUC={}".format(sub, accuracy_score(y, score)))
