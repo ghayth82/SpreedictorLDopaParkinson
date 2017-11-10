fda059692273a310add2576e3d6df188b1363a18
wkopp <kopp@molgen.mpg.de>
2017-11-10 11:58:28 +0100

    changed featurizer for submission

--- code/ldopa/featurizer.py
+++ code/ldopa/featurizer.py
@@ -12,10 +12,17 @@ outputdir = os.getenv('PARKINSON_DREAM_LDOPA_DATA')
 
 
 class Featurizer(object):
-    submission_comb = {"tre":('raw-tre-all', 'metatime_deep_conv', 'allaug'),
-            "bra": ('raw-bra-all', 'metatime_deep_conv', 'allaug'),
-            "dys": ('raw-dys-all', 'metatime_deep_conv', 'allaug')}
-    synapse_id = {"tre":9606376, "bra": 9606377, "dys": 9606378}
+    submission_comb = {
+            "tre1":('fh_0.5-tre-all', 'metatime_deep_conv_v2', 'allaug'),
+            "tre2":('raw-tre-all', 'metatime_deep_conv_v2', 'allaug'),
+            "bra1": ('raw-bra-all', 'metatime_deep_conv_v2', 'allaug_v2'),
+            "bra2": ('fh_0.5-bra-all', 'metatime_deep_conv_v2', 'allaug'),
+            "dys1": ('raw-dys-all', 'metatime_conv2l_70_200_10_50_30_20_10', 'allaug'),
+            "dys2": ('raw-dys-all', 'metatime_deep_conv_v2', 'allaug')}
+    synapse_id = {
+        "tre1":9606376, "bra1": 9606378, "dys1": 9606377,
+        "tre2":9606376, "bra2": 9606378, "dys2": 9606377,
+}
 
 
     '''
@@ -39,6 +46,8 @@ class Featurizer(object):
         # determine
 
         sub_comb = self.submission_comb[subch]
+        name = '.'.join(sub_comb[:2])
+        name = '_'.join([name, sub_comb[2]])
 
         da = {}
         X = {}
@@ -48,8 +57,6 @@ class Featurizer(object):
 
 
 
-        name = '.'.join(sub_comb[:2])
-        name = '_'.join([name, sub_comb[2]])
 
         model = Classifier(da, modeldefs[sub_comb[1]],
             comb=sub_comb, name=name, epochs = 1)
@@ -67,9 +74,10 @@ class Featurizer(object):
                     itertools.product(['feat'], range(features.shape[1]))]))
 
         pdfeatures.to_csv(os.path.join(self.summary_path,
-                "submission_{}_{}.csv".format(subch, version)),
+                "submission_{}_{}.csv".format(subch, name)),
                 sep = ",")
-        print("Submission file written to {}".format("submission_{}_{}.csv".format(subch, version)))
+        print("Submission file written to {}".format(
+            "submission_{}_{}.csv".format(subch, name)))
 
     def submit(self, subch, version):
         folderid = 'syn11389770'
@@ -77,6 +85,10 @@ class Featurizer(object):
         import synapseclient
         from synapseclient import File, Evaluation
         syn = synapseclient.login()
+
+        sub_comb = self.submission_comb[subch]
+        version = '.'.join(sub_comb[:2])
+        version = '_'.join([version, sub_comb[2]])
 
         name = "submission_{}_{}.csv".format(subch, version)
 
@@ -99,36 +111,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = \
             'Featurize the dataset and submit to challenge board', formatter_class = \
             RawTextHelpFormatter)
-    parser.add_argument('--gen_tre_feat', dest="trefeat",
-            default=False, action='store_true', help = "Generate features for tremor submission.")
-    parser.add_argument('--gen_bra_feat', dest="brafeat",
-            default=False, action='store_true', help = "Generate features for bradykinesia submission.")
-    parser.add_argument('--gen_dys_feat', dest="dysfeat",
-            default=False, action='store_true', help = "Generate features for dyskinesia submission.")
+    parser.add_argument('features', choices = [ k for k in Featurizer.submission_comb.keys()],
+            help = "Selection of features")
 
+    parser.add_argument('--gen', dest="gen",
+            default=False, action='store_true', help = "Generate features")
 
-    parser.add_argument('--submit_tre', dest="submit_tre",
-            default=False, action='store_true', help = "Submit tremor feature set.")
-    parser.add_argument('--submit_bra', dest="submit_bra",
-            default=False, action='store_true', help = "Submit bradykinesia feature set.")
-    parser.add_argument('--submit_dys', dest="submit_dys",
-            default=False, action='store_true', help = "Submit dyskinesia feature set.")
+    parser.add_argument('--submit', dest="submit",
+            default=False, action='store_true', help = "Submit feature set.")
 
     parser.add_argument('-version', dest='version', default = 'v1',
         help = "Version tag for the submission")
 
     args = parser.parse_args()
     fe = Featurizer()
-    if args.trefeat:
-        fe.generateSubmission("tre", args.version)
-    if args.brafeat:
-        fe.generateSubmission("bra", args.version)
-    if args.dysfeat:
-        fe.generateSubmission("dys", args.version)
+    if args.gen:
+        fe.generateSubmission(args.features, args.version)
 
-    if args.submit_tre:
-        fe.submit("tre", args.version)
-    if args.submit_bra:
-        fe.submit("bra", args.version)
-    if args.submit_dys:
-        fe.submit("dys", args.version)
+    if args.submit:
+        fe.submit(args.features, args.version)
